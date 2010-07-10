c8a355b16eaeefcd44378f571882d10fc537bca1
Kilian <kilian@kilianvalkhof.com>
2010-07-10 13:29:14 +0200

    implement color temperature, rename variables

--- src/fluxgui/fluxgui.py
+++ src/fluxgui/fluxgui.py
@@ -19,19 +19,21 @@ class Fluxgui:
         if self.settings.latitude is "" and self.settings.zipcode is "":
             self.open_preferences("activate")
 
-        self.start_xflux(self.settings.latitude, self.settings.zipcode)
+        self.start_xflux(self.settings.latitude, self.settings.zipcode, self.settings.color)
 
-    def start_xflux(self, latitude, zipcode):
-        command = "-z"
-        value = zipcode
+    def start_xflux(self, latitude, zipcode, color):
+        loccommand = "-z"
+        locvalue = zipcode
+
+        colcommand = "-k"
+        colvalue = "color"
 
         if self.settings.latitude:
-            command = "-l"
-            value = latitude
+            loccommand = "-l"
+            locvalue = latitude
 
-        args = [command, value, '-nofork']
+        args = [loccommand, locvalue, colcommand, colvalue, '-nofork']
         self.xflux = pexpect.spawn("xflux", args)
-        self.xflux.logfile = None
 
     def stop_xflux(self, item):
         self.indicator.item_turn_off.hide()
@@ -109,7 +111,7 @@ class Indicator:
         item.show()
         menu.append(item)
 
-        item = gtk.MenuItem("Exit")
+        item = gtk.MenuItem("Quit")
         item.connect("activate", self.main.exit)
         item.show()
         menu.append(item)
@@ -131,13 +133,16 @@ class Preferences:
         self.window = self.wTree.get_widget("window1")
         self.window.connect("destroy", self.delete_event)
 
-        self.input = self.wTree.get_widget("entry1")
-        self.input.set_text(self.main.settings.latitude)
-        self.input.connect("activate", self.delete_event)
+        self.latsetting = self.wTree.get_widget("entry1")
+        self.latsetting.set_text(self.main.settings.latitude)
+        self.latsetting.connect("activate", self.delete_event)
 
-        self.input2 = self.wTree.get_widget("entry2")
-        self.input2.set_text(self.main.settings.zipcode)
-        self.input2.connect("activate", self.delete_event)
+        self.zipsetting = self.wTree.get_widget("entry2")
+        self.zipsetting.set_text(self.main.settings.zipcode)
+        self.zipsetting.connect("activate", self.delete_event)
+
+        self.colsetting = self.wTree.get_widget("combobox1")
+        self.colsetting.set_active(int(self.main.settings.colortemp));
 
         if self.main.settings.latitude is ""\
            and self.main.settings.zipcode is "":
@@ -154,11 +159,14 @@ class Preferences:
             self.window.show()
 
     def delete_event(self, widget, data=None):
-        if self.main.settings.latitude != self.input.get_text():
-            self.main.settings.set_latitude(self.input.get_text())
+        if self.main.settings.latitude != self.latsetting.get_text():
+            self.main.settings.set_latitude(self.latsetting.get_text())
 
-        if self.main.settings.zipcode != self.input2.get_text():
-            self.main.settings.set_zipcode(self.input2.get_text())
+        if self.main.settings.zipcode != self.zipsetting.get_text():
+            self.main.settings.set_zipcode(self.zipsetting.get_text())
+
+        if self.main.settings.colortemp != str(self.colsetting.get_active()):
+            self.main.settings.set_colortemp(str(self.colsetting.get_active()))
 
         self.window.hide()
         return False
@@ -174,14 +182,21 @@ class Settings:
         self.client = gconf.client_get_default()
         self.prefs_key = "/apps/fluxgui"
         self.client.add_dir(self.prefs_key, gconf.CLIENT_PRELOAD_NONE)
+
         self.latitude = self.client.get_string(self.prefs_key + "/latitude")
         self.zipcode = self.client.get_string(self.prefs_key + "/zipcode")
+        self.colortemp = self.client.get_string(self.prefs_key + "/colortemp")
+        self.color = self.get_color(self.colortemp)
 
         if self.latitude is None:
             self.latitude = ""
 
         if self.zipcode is None:
             self.zipcode = ""
+
+        if not self.colortemp:
+            self.colortemp = "1"
+
 
     def set_latitude(self, latitude):
         self.client.set_string(self.prefs_key + "/latitude", latitude)
@@ -195,6 +210,28 @@ class Settings:
         self.zipcode = zipcode
 
         command = "z=" + zipcode
+        self.main.update_xflux(command)
+
+    def get_color(self, colortemp):
+        color = "3400"
+        if colortemp is "0":
+          color = "2700"
+        elif colortemp is "1":
+          color = "3400"
+        elif colortemp is "2":
+          color = "4200"
+        elif colortemp is "3":
+          color = "5000"
+
+        return color
+
+    def set_colortemp(self, colortemp):
+        color = self.get_color(colortemp)
+
+        self.client.set_string(self.prefs_key + "/colortemp", colortemp)
+        self.colortemp = colortemp
+
+        command = "k=" + color
         self.main.update_xflux(command)
 
     def main(self):

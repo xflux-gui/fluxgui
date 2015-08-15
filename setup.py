657dfb14145dc98d4dafde741abd2ac56846251c
Nathan Rennie-Waldock <nathan.renniewaldock@gmail.com>
2015-08-15 23:08:46 +0100

    debian: Automatically download xflux binary on install

--- setup.py
+++ setup.py
@@ -3,13 +3,20 @@
 from distutils.core import setup
 from sys import maxsize
 from shutil import copy
+import os
 
-# Determines which is the appropriate executable for 32-bit
-if maxsize == 2147483647:
-    copy("xflux32", "xflux")
-# ... or 64-bit processors
-elif maxsize == 9223372036854775807:
-    copy("xflux64", "xflux")
+data_files = [('share/icons/hicolor/scalable/apps', ['fluxgui.svg', 'fluxgui-light.svg', 'fluxgui-dark.svg']),
+('share/applications', ['desktop/fluxgui.desktop'])]
+
+if (os.path.exists("xflux32") or os.path.exists("xflux64")):
+    # Determines which is the appropriate executable for 32-bit
+    if maxsize == 2147483647:
+        copy("xflux32", "xflux")
+    # ... or 64-bit processors
+    elif maxsize == 9223372036854775807:
+        copy("xflux64", "xflux")
+    data_files.append( ('bin', ['xflux']) )
+
 
 setup(name = "f.lux indicator applet",
     version = "1.1.8",
@@ -21,9 +28,7 @@ setup(name = "f.lux indicator applet",
     package_dir = {'fluxgui' : 'src/fluxgui'},
     packages = ["fluxgui",],
     package_data = {"fluxgui" : ["*.glade"] },
-    data_files=[('share/icons/hicolor/scalable/apps', ['fluxgui.svg', 'fluxgui-light.svg', 'fluxgui-dark.svg']),
-            ('share/applications', ['desktop/fluxgui.desktop']),
-            ('bin', ['xflux']),],
+    data_files=data_files,
     scripts = ["fluxgui"],
     long_description = """f.lux indicator applet is an indicator applet to
     control xflux, an application that makes the color of your computer's

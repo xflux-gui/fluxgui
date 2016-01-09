5e65e0dc1c6d775c22f488997f67dcb9f067f810
Agustin Ferrari <agustinferrari@eml.cc>
2016-01-08 23:11:56 -0300

    Added 32-bit xflux executable in setup.py and fix some bugs

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from sys import maxsize
+from os import rename
+
+# Determines which is the appropriate executable for 32-bit
+if maxsize == 2147483647:
+    rename("xflux32", "xflux")
+# ... or 64-bit processors
+elif maxsize == 9223372036854775807:
+    rename("xflux64", "xflux")
 
 setup(name = "f.lux indicator applet",
     version = "1.1.8",

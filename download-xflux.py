5e27b6166a205239b5d075f5568ca5edf86051cb
Nathan Collins <nathan.collins@gmail.com>
2017-01-16 21:34:15 -0800

    Make download script work with Python 3.
    Closes #47.

--- download-xflux.py
+++ download-xflux.py
@@ -6,10 +6,10 @@ import os
 def download_xflux():
     # Determines which is the appropriate executable for 32-bit
     if maxsize == 2147483647:
-        print "Downloading 32-bit xflux ..."
+        print("Downloading 32-bit xflux ...")
         url = "https://justgetflux.com/linux/xflux-pre.tgz"
     elif maxsize == 9223372036854775807:
-        print "Downloading 64-bit xflux ..."
+        print("Downloading 64-bit xflux ...")
         url = "https://justgetflux.com/linux/xflux64.tgz"
     tarchive = "/tmp/xflux.tgz"
     os.system("wget '%s' -O'%s'" % (url, tarchive))

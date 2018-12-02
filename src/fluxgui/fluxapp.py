94a94533e13f6dc8f169d824d8610cc6b54948c6
LaBatata101 <labatata101@linuxmail.org>
2018-12-02 18:01:12 -0300

    removed old gtk+ 2 code

--- src/fluxgui/fluxapp.py
+++ src/fluxgui/fluxapp.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python
 
-from fluxgui import fluxcontroller, settings
-from fluxgui.exceptions import MethodUnavailableError
+import signal
+import os
+import sys
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
-import sys
-import os
-import signal
+from gi.repository import Gtk as gtk
+from fluxgui import fluxcontroller, settings
+from fluxgui.exceptions import MethodUnavailableError
+
 
 class FluxGUI(object):
     """
     FluxGUI initializes/destroys the app
     """
+
     def __init__(self):
         try:
             self.settings = settings.Settings()
             self.xflux_controller = fluxcontroller.FluxController(self.settings)
             self.indicator = Indicator(self, self.xflux_controller)
             self.preferences = Preferences(self.settings,
-                    self.xflux_controller)
+                                           self.xflux_controller)
             self.xflux_controller.start()
 
         except Exception as e:
@@ -50,6 +52,7 @@ class FluxGUI(object):
     def run(self):
         gtk.main()
 
+
 class Indicator(object):
     """
     Information and methods related to the indicator applet.
@@ -60,9 +63,9 @@ class Indicator(object):
         self.fluxgui = fluxgui
         self.xflux_controller = xflux_controller
         self.indicator = appindicator.Indicator.new(
-          "fluxgui-indicator",
-          "fluxgui",
-          appindicator.IndicatorCategory.APPLICATION_STATUS)
+            "fluxgui-indicator",
+            "fluxgui",
+            appindicator.IndicatorCategory.APPLICATION_STATUS)
 
         self.setup_indicator()
 
@@ -75,7 +78,7 @@ class Indicator(object):
         menu = gtk.Menu()
 
         self.add_menu_item("_Pause f.lux", self._toggle_pause,
-                menu, MenuItem=gtk.CheckMenuItem)
+                           menu, MenuItem=gtk.CheckMenuItem)
         self.add_menu_item("Prefere_nces", self._open_preferences, menu)
         self.add_menu_separator(menu)
         self.add_menu_item("Quit", self._quit, menu)
@@ -83,7 +86,7 @@ class Indicator(object):
         return menu
 
     def add_menu_item(self, label, handler, menu,
-            event="activate", MenuItem=gtk.MenuItem, show=True):
+                      event="activate", MenuItem=gtk.MenuItem, show=True):
         item = MenuItem(label)
         item.connect(event, handler)
         menu.append(item)
@@ -101,10 +104,11 @@ class Indicator(object):
         self.xflux_controller.toggle_pause()
 
     def _open_preferences(self, item):
-            self.fluxgui.open_preferences()
+        self.fluxgui.open_preferences()
 
     def _quit(self, item):
         self.fluxgui.exit()
+
 
 class Preferences(object):
     """
@@ -114,34 +118,33 @@ class Preferences(object):
     """
 
     def connect_widget(self, widget_name, connect_target=None,
-            connect_event="activate"):
+                       connect_event="activate"):
         widget = self.wTree.get_object(widget_name)
         if connect_target:
             widget.connect(connect_event, connect_target)
         return widget
-
 
     def __init__(self, settings, xflux_controller):
         self.settings = settings
         self.xflux_controller = xflux_controller
 
         self.gladefile = os.path.join(os.path.dirname(os.path.dirname(
-          os.path.realpath(__file__))), "fluxgui/preferences.glade")
-        self.wTree = Gtk.Builder.new_from_file(self.gladefile)
+            os.path.realpath(__file__))), "fluxgui/preferences.glade")
+        self.wTree = gtk.Builder.new_from_file(self.gladefile)
 
         self.window = self.connect_widget("window1", self.delete_event,
-                connect_event="destroy")
+                                          connect_event="destroy")
         self.latsetting = self.connect_widget("entryLatitude",
-                self.delete_event)
+                                              self.delete_event)
         self.lonsetting = self.connect_widget("entryLongitude",
-                self.delete_event)
+                                              self.delete_event)
         self.zipsetting = self.connect_widget("entryZipcode",
-                self.delete_event)
+                                              self.delete_event)
         self.colsetting = self.connect_widget("comboColor")
         self.previewbutton = self.connect_widget("buttonPreview",
-                self.preview_click_event, "clicked")
+                                                 self.preview_click_event, "clicked")
         self.closebutton = self.connect_widget("buttonClose",
-                self.delete_event, "clicked")
+                                               self.delete_event, "clicked")
         self.autostart = self.connect_widget("checkAutostart")
 
         if (self.settings.latitude is "" and self.settings.zipcode is "")\
@@ -165,11 +168,11 @@ class Preferences(object):
 
     def display_no_zipcode_or_latitude_error_box(self):
         md = gtk.MessageDialog(self.window,
-                gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_INFO,
-                gtk.BUTTONS_OK, "The f.lux indicator applet needs to know " +
-                "your latitude or zipcode to run. " +
-                "Please fill either of them in on the preferences screen "+
-                "and click 'Close'.")
+                               gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_INFO,
+                               gtk.BUTTONS_OK, ("The f.lux indicator applet needs to know "
+                                                "your latitude or zipcode to run. "
+                                                "Please fill either of them in on "
+                                                "the preferences screen and click 'Close'."))
         md.set_title("f.lux indicator applet")
         md.run()
         md.destroy()
@@ -182,18 +185,18 @@ class Preferences(object):
     def delete_event(self, widget, data=None):
         if self.settings.latitude != self.latsetting.get_text():
             self.xflux_controller.set_xflux_latitude(
-                    self.latsetting.get_text())
+                self.latsetting.get_text())
 
         if self.settings.longitude != self.lonsetting.get_text():
             self.xflux_controller.set_xflux_longitude(
-                    self.lonsetting.get_text())
+                self.lonsetting.get_text())
 
         if self.settings.zipcode != self.zipsetting.get_text():
             self.xflux_controller.set_xflux_zipcode(
-                    self.zipsetting.get_text())
+                self.zipsetting.get_text())
 
         colsetting_temperature = settings.key_to_temperature(
-                self.colsetting.get_active())
+            self.colsetting.get_active())
         if self.settings.color != colsetting_temperature:
             self.xflux_controller.color = colsetting_temperature
 
@@ -209,6 +212,7 @@ class Preferences(object):
         self.window.hide()
         return False
 
+
 def main():
     try:
         app = FluxGUI()
@@ -219,6 +223,7 @@ def main():
         # No idea why we consistently get a keyboard interrupt here
         # after killing fluxgui with SIGINT or SIGTERM ...
         pass
+
 
 if __name__ == '__main__':
     main()

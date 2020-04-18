9cd2a4235768849c7388cc835ee21e70095ae9ea
LaBatata101 <labatata101@linuxmail.org>
2020-04-18 20:53:05 -0300

    add gtk.Notebook with two tabs

--- src/fluxgui/fluxapp.py
+++ src/fluxgui/fluxapp.py
@@ -2,6 +2,9 @@
 
 from fluxgui.exceptions import MethodUnavailableError
 from fluxgui import fluxcontroller, settings
+from fluxgui.redshiftcontroller import RedshiftSettings
+from fluxgui.xfluxpage import XfluxPage
+from fluxgui.redshiftpage import RedshiftPage
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk as gtk
@@ -20,9 +23,10 @@ class FluxGUI(object):
         try:
             self.settings = settings.Settings()
             self.xflux_controller = fluxcontroller.FluxController(self.settings)
+            self.redshift_controller = RedshiftSettings(self.settings)
             self.indicator = Indicator(self, self.xflux_controller)
             self.preferences = Preferences(self.settings,
-                    self.xflux_controller)
+                    self.xflux_controller, self.redshift_controller)
             self.xflux_controller.start()
 
         except Exception as e:
@@ -103,7 +107,7 @@ class Indicator(object):
         self.xflux_controller.toggle_pause()
 
     def _open_preferences(self, item):
-            self.fluxgui.open_preferences()
+        self.fluxgui.open_preferences()
 
     def _quit(self, item):
         self.fluxgui.exit()
@@ -122,47 +126,39 @@ class Preferences(object):
             widget.connect(connect_event, connect_target)
         return widget
 
+    def switch_page(self, *args):
+        # the NoteBook page index is the last element of the list
+        if args[2] == 0:  # Xflux page
+            self.xflux_page.show()
+        elif args[2] == 1:  # Redshift page
+            self.redshift_page.show()
 
-    def __init__(self, settings, xflux_controller):
+    def __init__(self, settings, xflux_controller, redshift_controller):
         self.settings = settings
-        self.xflux_controller = xflux_controller
+        # self.xflux_controller = xflux_controller
 
         self.gladefile = os.path.join(os.path.dirname(os.path.dirname(
           os.path.realpath(__file__))), "fluxgui/preferences.glade")
         self.wTree = gtk.Builder.new_from_file(self.gladefile)
 
-        self.window = self.connect_widget("window1", self.delete_event,
+        # self.window = self.connect_widget("window1", self.delete_event,
+        self.window = self.connect_widget("window1", gtk.main_quit,
                 connect_event="delete-event")
-        self.latsetting = self.connect_widget("entryLatitude",
-                self.delete_event)
-        self.lonsetting = self.connect_widget("entryLongitude",
-                self.delete_event)
-        self.zipsetting = self.connect_widget("entryZipcode",
-                self.delete_event)
-        self.colsetting = self.connect_widget("comboColor")
-        self.previewbutton = self.connect_widget("buttonPreview",
-                self.preview_click_event, "clicked")
-        self.closebutton = self.connect_widget("buttonClose",
-                self.delete_event, "clicked")
-        self.autostart = self.connect_widget("checkAutostart")
+        self.notebook = self.connect_widget("notebook", self.switch_page,
+                                            connect_event="switch-page")
+        self.xflux_page = XfluxPage(self.window, xflux_controller, self.settings,
+                                    self.connect_widget)
+        self.redshift_page = RedshiftPage(self.window, redshift_controller, self.settings,
+                                          self.connect_widget)
+        self.default_page = self.xflux_page
 
-        if (self.settings.latitude is "" and self.settings.zipcode is "")\
+        if (self.settings.latitude == "" and self.settings.zipcode == "")\
                 or not self.settings.has_set_prefs:
             self.show()
             self.display_no_zipcode_or_latitude_error_box()
 
     def show(self):
-
-        self.latsetting.set_text(self.settings.latitude)
-        self.lonsetting.set_text(self.settings.longitude)
-        self.zipsetting.set_text(self.settings.zipcode)
-        self.colsetting.set_active(settings.temperature_to_key(self.settings.color))
-
-        if self.settings.autostart:
-            self.autostart.set_active(True)
-        else:
-            self.autostart.set_active(False)
-
+        self.default_page.show()
         self.window.show()
 
     def display_no_zipcode_or_latitude_error_box(self):
@@ -175,41 +171,6 @@ class Preferences(object):
         md.set_title("f.lux indicator applet")
         md.run()
         md.destroy()
-
-    def preview_click_event(self, widget, data=None):
-        colsetting_temperature = settings.key_to_temperature(
-            self.colsetting.get_active())
-        self.xflux_controller.preview_color(colsetting_temperature)
-
-    def delete_event(self, widget, data=None):
-        if self.settings.latitude != self.latsetting.get_text():
-            self.xflux_controller.set_xflux_latitude(
-                    self.latsetting.get_text())
-
-        if self.settings.longitude != self.lonsetting.get_text():
-            self.xflux_controller.set_xflux_longitude(
-                    self.lonsetting.get_text())
-
-        if self.settings.zipcode != self.zipsetting.get_text():
-            self.xflux_controller.set_xflux_zipcode(
-                    self.zipsetting.get_text())
-
-        colsetting_temperature = settings.key_to_temperature(
-                self.colsetting.get_active())
-        if self.settings.color != colsetting_temperature:
-            self.xflux_controller.color = colsetting_temperature
-
-        if self.autostart.get_active():
-            self.xflux_controller.set_autostart(True)
-        else:
-            self.xflux_controller.set_autostart(False)
-        if self.latsetting.get_text() == "" \
-                and self.zipsetting.get_text() == "":
-            self.display_no_zipcode_or_latitude_error_box()
-            return True
-
-        self.window.hide()
-        return True
 
 def main():
     try:

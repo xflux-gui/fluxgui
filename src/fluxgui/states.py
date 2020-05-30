555afe37086f391c1d7382b0c25445e9fac151d4
LaBatata101 <labatata101@linuxmail.org>
2020-05-30 15:05:52 -0300

    rename _XfluxState class to _State use format function to format exception strings

--- src/fluxgui/states.py
+++ src/fluxgui/states.py
@@ -2,33 +2,35 @@ from fluxgui.exceptions import MethodUnavailableError
 import weakref
 
 
-class _XfluxState(object):
+class _State(object):
     can_change_settings = False
 
     def __init__(self, controller_instance):
+        self.name = str(controller_instance)
         self.controller_ref = weakref.ref(controller_instance)
 
     def start(self, startup_args):
         raise MethodUnavailableError(
-                "Xflux cannot start in its current state")
+                "{} cannot start in its current state".format(self.name))
 
     def stop(self):
         raise MethodUnavailableError(
-                "Xflux cannot stop in its current state")
+                "{} cannot stop in its current state".format(self.name))
 
     def preview(self, preview_color):
         raise MethodUnavailableError(
-                "Xflux cannot preview in its current state")
+                "{} cannot preview in its current state".format(self.name))
 
     def toggle_pause(self):
         raise MethodUnavailableError(
-                "Xflux cannot pause/unpause in its current state")
+                "{} cannot pause/unpause in its current state".format(self.name))
 
     def set_setting(self, **kwargs):
         raise MethodUnavailableError(
-                "Xflux cannot alter settings in its current state")
+                "{} cannot alter settings in its current state".format(self.name))
 
-class _InitState(_XfluxState):
+
+class _InitState(_State):
     def start(self, startup_args):
         self.controller_ref()._start(startup_args)
         self.controller_ref().state = self.controller_ref().states["RUNNING"]
@@ -40,12 +42,13 @@ class _InitState(_XfluxState):
         for key, value in kwargs.items():
             self.controller_ref().init_kwargs[key] = str(value)
 
-class _TerminatedState(_XfluxState):
+
+class _TerminatedState(_State):
     def stop(self):
         return True
 
 
-class _AliveState(_XfluxState):
+class _AliveState(_State):
     can_change_settings = True
 
     def stop(self):
@@ -62,19 +65,20 @@ class _AliveState(_XfluxState):
 class _RunningState(_AliveState):
     def toggle_pause(self):
         self.controller_ref()._change_color_immediately(
-                self.controller_ref()._pause_color)
+            self.controller_ref()._pause_color)
         self.controller_ref().state = self.controller_ref().states["PAUSED"]
 
     def preview(self, preview_color):
         self.controller_ref()._preview_color(preview_color,
-                self.controller_ref()._current_color)
+                                             self.controller_ref()._current_color)
 
 
 class _PauseState(_AliveState):
     def toggle_pause(self):
         self.controller_ref()._change_color_immediately(
-                self.controller_ref()._current_color)
+            self.controller_ref()._current_color)
         self.controller_ref().state = self.controller_ref().states["RUNNING"]
+
     def preview(self, preview_color):
         self.controller_ref()._preview_color(preview_color,
-                self.controller_ref()._pause_color)
+                                             self.controller_ref()._pause_color)

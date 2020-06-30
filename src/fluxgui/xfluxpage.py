4f19402fa49df8d5d1f36a76634ae226aca7ce00
LaBatata101 <labatata101@linuxmail.org>
2020-06-30 19:59:11 -0300

    add controllers to constructor parameter add _activate_xflux method add save_changes method only close Preferences window if latitude or longitude are set otherwise show aler

--- src/fluxgui/xfluxpage.py
+++ src/fluxgui/xfluxpage.py
@@ -1,11 +1,15 @@
 from fluxgui import settings
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk as gtk
 
 
 class XfluxPage:
-    def __init__(self, window, xflux_controller, settings, connect_widget):
+    def __init__(self, window, controllers, settings, connect_widget):
         self.window = window
         self.settings = settings
-        self.xflux_controller = xflux_controller
+        self.xflux_controller = controllers["xflux"]
+        self.redshift_controller = controllers["redshift"]
         self.latsetting = connect_widget("entryLatitude1",
                                          self.delete_event)
         self.lonsetting = connect_widget("entryLongitude1",
@@ -18,14 +22,17 @@ class XfluxPage:
         self.closebutton = connect_widget("buttonClose1",
                                           self.delete_event, "clicked")
         self.autostart = connect_widget("checkAutostart1")
-        self.checkXflux = connect_widget("checkXflux", self._switch_to_xflux, "toggled")
+        self.checkXflux = connect_widget("checkXflux", self._activate_xflux, "toggled")
 
-    def _switch_to_xflux(self, *args):
-        if args[0].get_active():
-            self.settings.use_xflux = True
-            self.settings.use_redshift = False
-        else:
-            self.settings.use_xflux = False
+    def _activate_xflux(self, *args):
+        # args[0] is the gtkCheckButton object
+        self.settings.use_xflux = args[0].get_active()
+        self.settings.use_redshift = not args[0].get_active()
+        if self.settings.use_xflux and \
+           self.xflux_controller.state != self.xflux_controller.states["RUNNING"]:
+            self.redshift_controller.stop()
+            self.xflux_controller.state = self.xflux_controller.states["INIT"]
+            self.xflux_controller.start()
 
     def show(self):
         self.latsetting.set_text(self.settings.latitude)
@@ -33,28 +40,21 @@ class XfluxPage:
         self.zipsetting.set_text(self.settings.zipcode)
         self.colsetting.set_active(settings.temperature_to_key(self.settings.color))
 
-        if self.settings.use_xflux:
-            self.checkXflux.set_active(True)
-        else:
-            self.checkXflux.set_active(False)
-
-        if self.settings.autostart:
-            self.autostart.set_active(True)
-        else:
-            self.autostart.set_active(False)
+        self.checkXflux.set_active(self.settings.use_xflux)
+        self.autostart.set_active(self.settings.autostart)
 
     def preview_click_event(self, widget, data=None):
         colsetting_temperature = settings.key_to_temperature(
             self.colsetting.get_active())
         self.xflux_controller.preview_color(colsetting_temperature)
 
-    def delete_event(self, widget, data=None):
+    def save_changes(self):
         if self.settings.latitude != self.latsetting.get_text():
-            self.xflux_controller.set_latitude(
+            self.xflux_controller.set_xflux_latitude(
                 self.latsetting.get_text())
 
         if self.settings.longitude != self.lonsetting.get_text():
-            self.xflux_controller.set_longitude(
+            self.xflux_controller.set_xflux_longitude(
                 self.lonsetting.get_text())
 
         if self.settings.zipcode != self.zipsetting.get_text():
@@ -66,14 +66,27 @@ class XfluxPage:
         if self.settings.color != colsetting_temperature:
             self.xflux_controller.color = colsetting_temperature
 
-        if self.autostart.get_active():
-            self.xflux_controller.set_autostart(True)
-        else:
-            self.xflux_controller.set_autostart(False)
-        if self.latsetting.get_text() == "" \
-                and self.zipsetting.get_text() == "":
+        self.xflux_controller.set_autostart(self.autostart.get_active())
+
+    def is_latitude_or_zipcode_set(self):
+        return self.latsetting.get_text() or self.zipsetting.get_text()
+
+    def delete_event(self, widget, data=None):
+        if not self.is_latitude_or_zipcode_set():
             self.display_no_zipcode_or_latitude_error_box()
             return True
 
+        self.save_changes()
         self.window.hide()
         return True
+
+    def display_no_zipcode_or_latitude_error_box(self):
+        md = gtk.MessageDialog(self.window,
+                               gtk.DialogFlags.DESTROY_WITH_PARENT, gtk.MessageType.INFO,
+                               gtk.ButtonsType.OK, ("The f.lux indicator applet needs to know "
+                                                    "your latitude or zipcode to run. "
+                                                    "Please fill either of them in on "
+                                                    "the preferences screen and click 'Close'."))
+        md.set_title("f.lux indicator applet")
+        md.run()
+        md.destroy()
